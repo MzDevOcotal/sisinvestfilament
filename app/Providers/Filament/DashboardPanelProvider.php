4aea1d8ad2b8e3feb767c7ab835ef4ecc37eb964
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-09-05 23:05:23 -0600

    feat: add Tailwind CSS configuration and theme integration
    - Introduced new theme.css and tailwind.config.js files.
    - Updated postcss.config.js to include Tailwind CSS plugins.
    - Modified vite.config.js to include the new theme in the build process.
    - Removed legacy styles from app.css.

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -46,6 +46,7 @@ class DashboardPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
 /*                 Widgets\AccountWidget::class,
